fd54bbccc00c94bef3ed617bb07a066675eb396d
keroken <kenichi.shinagawa@gmail.com>
2021-10-24 21:00:48 +0900

    add controlled input

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export const TodoForm = () => {
+  const [value, setValue] = useState('');
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
+    
   };
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" />
+      <input
+        type="text"
+        value={value}
+        onChange={e => handleChange(e)}
+      />
       <button>Submit</button>
     </form>
   );
