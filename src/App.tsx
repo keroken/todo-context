5b68cb47b748e6090bcbd00e7b4eb77767d9ef79
keroken <kenichi.shinagawa@gmail.com>
2021-10-26 23:19:12 +0900

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,15 @@
-import React, { createContext, useState } from 'react';
+import React from 'react';
+import TodoProvider from './state/TodoProvider';
 import { TodoList } from './components/TodoList';
 import { TodoForm } from './components/TodoForm';
-import { v4 } from 'uuid';
-
-export type todoType = {
-  id: string;
-  item: string;
-  isDone: boolean;
-}[];
-
-export type todoState = {
-  todos: todoType;
-  onNewTodo: (value: string) => void;
-};
-
-export const TodoContext = createContext<todoState>(
-  { todos: [], onNewTodo: () => [] }
-);
 
 function App() {
-  const [todos, setTodos] = useState<todoType>([]);
-  const onNewTodo = (value: string) => {
-    const newTodos = [
-      ...todos,
-      {
-        id: v4(),
-        item: value,
-        isDone: false
-      }
-    ];
-    setTodos(newTodos);
-  };
   return (
-    <TodoContext.Provider value={{ todos, onNewTodo }}>
+    <TodoProvider>
       <h3>Todo Context</h3>
       <TodoList />
       <TodoForm />
-    </TodoContext.Provider>
+    </TodoProvider>
   );
 }
 
