46cd9ffed1e9b9091967e491bd6ae4df4d1065a1
keroken <kenichi.shinagawa@gmail.com>
2021-10-27 00:28:25 +0900

    use useInput hook in TodoForm

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,23 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useTodos } from '../state/TodoProvider';
+import { useInput } from '../hooks/useInput';
 
 export const TodoForm = () => {
   const { onNewTodo } = useTodos();
-  const [value, setValue] = useState('');
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setValue(e.target.value);
-  };
+  const [valueProps, resetValue] = useInput('');
+
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    onNewTodo(value);
-    setValue('');
+    onNewTodo(valueProps.value);
+    resetValue();
   };
   return (
     <form onSubmit={handleSubmit}>
       <input
         type="text"
-        value={value}
-        onChange={e => handleChange(e)}
+        {...valueProps}
       />
       <button>Submit</button>
     </form>
