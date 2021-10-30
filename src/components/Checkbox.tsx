d9411909fa4e7109bd8027db9cbdd4b7c4c83d21
keroken <kenichi.shinagawa@gmail.com>
2021-10-30 22:45:11 +0900

    move set checked to function

--- src/components/Checkbox.tsx
+++ src/components/Checkbox.tsx
@@ -2,13 +2,16 @@ import React, { useState } from 'react';
 
 export const Checkbox = () => {
   const [checked, setChecked] = useState(false);
+  const handleToggle = () => {
+    setChecked(checked => !checked);
+  };
 
   return (
     <>
       <input
         type="checkbox"
         checked={checked}
-        onChange={() => setChecked(checked => !checked)}
+        onChange={handleToggle}
       />
     </>
   );
